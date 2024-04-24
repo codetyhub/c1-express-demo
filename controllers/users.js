8150bf1a827c0676f99ed89be252fab0e104f4d0
Patrick Niyogitare <patrickniyogitare28@gmail.com>
2024-04-24 20:47:40 +0200

    Added PUT & DELETE methods

--- controllers/users.js
+++ controllers/users.js
@@ -29,3 +29,37 @@ export const addUser = (req, res) => {
         message: "User created successfully"
     })
 }
+
+export const updateUser = (req, res) => {
+    const {name} = req.body;
+  
+    const userIndex = _userExists(req, res);
+    
+    users[userIndex].name = name;
+
+    return res.status(200).json({success: true, message: "User updated successfully"})
+}
+
+export const deleteUser = (req, res) => {
+    const userIndex = _userExists(req, res);
+    
+    users.splice(userIndex, 1)
+
+    return res.status(200).json({success: true, message: "User deleted successfully"})
+}
+
+export const getUserById = (req, res) => {
+    const userIndex = _userExists(req, res);
+
+    return res.status(200).json({success: true, data: {user: users[userIndex]}})
+}
+
+const _userExists = (req, res) => {
+    const {id} = req.params;
+
+    const userIndex = users.findIndex((user) => user.id == id)
+    
+    if(userIndex === -1) return res.status(404).json({success: false, message: `User with id [${id}] was not found`})
+
+    return userIndex;
+}
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,17 @@
 // routes/users.js
 import express from "express";
-import { getUsers, addUser } from "../controllers/users"; 
+import { getUsers, addUser, updateUser, deleteUser, getUserById } from "../controllers/users"; 
 
 const router = express.Router();
 
 router.get('/', getUsers);
 
 router.post('/', addUser)
+
+router.put('/:id', updateUser)
+
+router.delete('/:id', deleteUser)
+
+router.get('/:id', getUserById)
 
 export default router;
